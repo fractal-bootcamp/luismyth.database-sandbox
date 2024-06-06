c0ed1c8725002002a78480f943cea9ba3e11698a
yab <hello@factions.ai>
2024-06-06 16:52:55 -0400

    works

--- borrow-book.ts
+++ borrow-book.ts
@@ -1,62 +1,38 @@
-import { PrismaClient } from "@prisma/client"
-// for some reason defining a required input works but import does not...
+// import { PrismaClient } from "@prisma/client"
 
-// const { PrismaClient } = require("@prisma/client");
+const { PrismaClient } = require("@prisma/client");
+
 
 const prisma = new PrismaClient()
 
-// type AuthorData = {
-//     name: string;
-//     biography?: string;
-// }
+
 
 async function main() {
     console.log('the script is running')
-
+    
     const allAuthors = await prisma.author.findMany()
     console.log(allAuthors)
-
+    
     await prisma.author.create({
         data: {
-            name: "Kurt Vonnegut"
+            name: "Yet another Kurt Vonnegut"
+            }
+    })
+
+    console.log("finished");
+    
+    const borrowBook = await prisma.book.update({
+        where: {
+            id: 'bookid01234',
+        },
+        data: {
+            borrowerId: 'member987',
         }
     })
-    console.log("finished")
+    console.log(borrowBook)
 
+    console.log("all done")
+            
 }
-
+            
 main()
-
-
-// async function main() {
-//     let include
-// }
-
-
-// model Author {
-//   id        String    @id @default(cuid())
-//   name      String
-//   biography String?
-//   createdAt DateTime  @default(now())
-//   updatedAt DateTime? @updatedAt
-
-//   // Relationship info for Prisma client - NOT database columns
-//   books     Book[]
-
-//   @@map("author")
-// }
-
-// Initial example from https://www.prisma.io/docs/orm/prisma-client/queries/crud
-
-// const user = await prisma.author.create({
-//     data: {
-//         name: 'Kurt Vonnegut'
-//     }
-// })
-
-
-// function App () {
-//     user
-// }
-
-// export default App;
